2f38b88de1b4e38c99865675abc487c636b42e0c
guaracyalima <guaracyaraujolima@gmail.com>
2018-02-25 18:52:35 -0300

    full complete

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model'
+
+export const FRASES: Frase[] = [
+  { fraseEng: 'I like to learn', frasePtbr:  'Eu gosto de estudar' },
+  { fraseEng: 'I watch tv', frasePtbr:  'Eu assisto tv' },
+  { fraseEng: 'How are you?', frasePtbr:  'Como vai você' },
+  { fraseEng: 'I eat bread', frasePtbr:  'Eu como pão' }
+]
---
+++ src/app/shared/frase.model.ts
@@ -0,0 +1,9 @@
+export class Frase {
+  public fraseEng: string
+  public frasePtbr: string
+
+  constructor(fraseEng: string, frasePtbr: string){
+    this.fraseEng = fraseEng
+    this.frasePtbr = this.frasePtbr
+  }
+}
